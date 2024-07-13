2b5be2e8e7808a8d4a06d42d28054af0ca00449b
WenasRPG <rokirsammer@gmail.com>
2024-07-13 15:23:19 +0200

    fixed bugs

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -1,13 +1,30 @@
-{ config, lib, pkgs, inputs, pkgs-unstable , ... }:
+{ config, pkgs, inputs, ... }:
 
+let
+  unstable = import inputs.nixpkgs-unstable {
+    system = pkgs.system;
+    config.allowUnfree = true;
+  };
+in
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+  ];
 
-  # Use the systemd-boot EFI boot loader.
+  # System Configuration
+  system.stateVersion = "24.05";
+  time.timeZone = "Europe/Berlin";
+  networking = {
+    hostName = "xyfr-01";
+    networkmanager.enable = true;
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 22 ];
+    };
+  };
+
+  # Boot Configuration
   boot.loader = {
     grub = {
       enable = true;
@@ -17,60 +34,89 @@
     efi.canTouchEfiVariables = true;
   };
 
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-    
-    extraPackages = with pkgs; [
-      mesa
-      amdvlk
-    ];
-  };
-
-  virtualisation.libvirtd.enable = true;
-  virtualisation.libvirtd.qemu.package = pkgs.qemu;
-
-  services.xserver.videoDrivers = ["amdgpu"];
-
-  networking.hostName = "xyfr-01"; # Define your hostname.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
-
-  # Select internationalisation properties.
+  # Localization
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "de";
   };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  # Graphics and Display
+  services.xserver = {
+    enable = true;
+    videoDrivers = ["amdgpu"];
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    # Uncomment if needed:
+    # xkb.layout = "de";
+    # xkb.options = "eurosign:e,caps:escape";
+  };
 
- 
- 
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      mesa
+      amdvlk
+    ];
+  };
+
+  # Audio
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # Virtualization
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu.package = pkgs.qemu;
+  };
+
+  # System Services
+  services.printing.enable = true;
   security.polkit.enable = true;
 
+  # Nix Configuration
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
+  # User Configuration
+  users.users.xyfr = {
+    isNormalUser = true;
+    description = "xyfr";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" "realtime" ];
+    packages = with pkgs; [];
+  };
 
-    # General utilities
-    pkgs-unstable.firefox-devedition
-    zip
+  # Home Manager Configuration
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "xyfr" = import ./home.nix;
+    };
+  };
+
+  # System Packages
+  environment.systemPackages = with pkgs; [
+    # Browsers and Communication
+    unstable.firefox-devedition
+    chromium
+    discord
+    whatsapp-for-linux
+    tor-browser
+    zoom-us
+
+    # Media and Graphics
     spotify
     spotify-cli-linux
-    rsync
-    tree
-    wget
-    curl
-    jq
-    jdk8
-    jdk17
-    jdk21
-    ibus
-
-    # Media
     glaxnimate
     kdePackages.kdenlive
     vlc
@@ -80,24 +126,27 @@
     geogebra6
     gimp
 
-    # Communication
-    bitwarden-desktop
-    discord
-    whatsapp-for-linux
-    tor-browser
-    zoom-us
+    # Development
+    git
+    jdk8
+    jdk17
+    jdk21
+    neovim
 
-    # System tools
+    # System Utilities
+    zip
+    rsync
+    tree
+    wget
+    curl
+    jq
+    ibus
     dbus
     efibootmgr
     gparted
-    neovim
     kitty
-    chromium
-
-    # Fonts
-    corefonts
-    vistafonts
+    alsa-utils
+    fastfetch
 
     # Virtualization
     libvirt
@@ -105,85 +154,38 @@
     qemu
     swtpm
 
-    # Development
-    git
-    alsa-utils
+    # Wine
     wineWowPackages.unstableFull
     wineWowPackages.stagingFull
 
     # Miscellaneous
-    fastfetch
+    bitwarden-desktop
     ventoy
     onlyoffice-bin_latest
     blueman
     bluez
     bluez-tools
     qbittorrent
+
+    # Fonts
+    corefonts
+    vistafonts
   ];
 
-
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
+  # GNOME Configuration
   environment.gnome.excludePackages = (with pkgs; [
     gnome-photos
     gnome-tour
+    gnome-terminal
+    epiphany
+    geary
+    evince
+    totem
   ]) ++ (with pkgs.gnome; [
     gnome-music
-    gnome-terminal
-    epiphany # web browser
-    geary # email reader
-    evince # document viewer
-    totem # video player
-    tali # poker game
-    iagno # go game
-    hitori # sudoku game
-    atomix # puzzle game
-  ]);  
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "de";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  # If you want to use JACK applications, uncomment this
-    jack.enable = true;
-   };
-  
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.xyfr = { 
-    isNormalUser = true;
-    description = "xyfr"; 
-    extraGroups = [ "networkmanager" "wheel" "libvirtd" "realtime" ]; 
-    packages = with pkgs; [ 
-    ];
-  }; 
-
-  home-manager = {
-    extraSpecialArgs = { inherit inputs; };
-    users = { 
-      "xyfr" = import ./home.nix;
-    };
-  };
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
-  networking.firewall.enable = true;
-
-  system.stateVersion = "24.05"; # Did you read the comment?
-
+    tali
+    iagno
+    hitori
+    atomix
+  ]);
 }
